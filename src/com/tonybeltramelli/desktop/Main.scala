0674f5a67c9df34a83387a1ef21dfa0bf7aa3184
Tony Beltramelli <tbeltramelli@gmail.com>
2014-10-12 00:17:08 +0200

    rating

--- src/com/tonybeltramelli/desktop/Main.scala
+++ src/com/tonybeltramelli/desktop/Main.scala
@@ -7,6 +7,10 @@ import ch.ethz.dal.tinyir.processing.Tokenizer
 import com.github.aztek.porterstemmer.PorterStemmer
 import com.sun.xml.internal.bind.v2.TODO
 import scala.collection.mutable.PriorityQueue
+import ch.ethz.dal.tinyir.lectures.TipsterGroundTruth
+import scala.collection.immutable.Stream
+import java.io.InputStream
+import scala.io.Source
 
 object Main {
 	def main(args: Array[String])
@@ -17,36 +21,42 @@ object Main {
 
 class Main
 {
+	val ROOT_PATH = "data/tipster/"
+	val ZIP_PATH = ROOT_PATH + "/zips"
+	val QRELS_PATH = ROOT_PATH + "/qrels"
+	val TOPIC_PATH = ROOT_PATH + "/topics"
+	
 	def this(resultNumber: Int, queries: List[String])
 	{
 	  this()
 	  
 	  val time = System.nanoTime()
 	  
-	  val tipster = new TipsterStream("data/tipster/zips")
-	  
-	  val queriesTokens = _stemTokens(queries).map(q => Tokenizer.tokenize(q))
+	  val tipster = new TipsterStream(ZIP_PATH)	  
+	  val queriesTokens = queries.map(q => Tokenizer.tokenize(q)).map(t => _stemTokens(t))
 	  
 	  println("time 0 : " + (System.nanoTime() - time) / 1000000000.0 + " seconds")
 	  
 	  prt("queries : "+queries.mkString(", "))
 	  prt("documents : "+tipster.length)
 
-	  val docs = tipster.stream.take(1000)
+	  val docs = tipster.stream.take(10)
 	  
 	  for (query <- queriesTokens)
 	  {
-		  //val scores = docs.map(doc => doc.name -> _getScore(_stemTokens(doc.tokens), query))//.sortBy(-_._2)
+		  val priorityQ = new PriorityQueue[(String, Double)]()(Ordering.by(_ordering))
+		  docs.foreach(d => priorityQ.enqueue((d.name, _getScore(_stemTokens(d.tokens), query))))
 		  
-		  val pQ = new PriorityQueue[(String, Double)]()(Ordering.by(_ordering))
-		  docs.foreach(d => pQ.enqueue((d.name, _getScore(_stemTokens(d.tokens), query))))
-		  
-		  val result = pQ.take(resultNumber)
+		  val result = priorityQ.take(resultNumber)
 		  
 		  println("results for \""+query+"\" : "+result.mkString(", "))
+		  println("time 1 : " + (System.nanoTime() - time) / 1000000000.0 + " seconds")
+		  
+		  val topics = _getTopics
+		  
 	  }
 	  
-	  println("time 1 : " + (System.nanoTime() - time) / 1000000000.0 + " seconds")
+	  println("time 2 : " + (System.nanoTime() - time) / 1000000000.0 + " seconds")
 	}
 	
 	private def _getTermFreq(list : List[String]) : Map[String,Int] =
@@ -75,6 +85,15 @@ class Main
 	private def _stemTokens(list: List[String]) : List[String] = 
 	{
 	  list.map(t => t.toLowerCase()).map(PorterStemmer.stem(_))
+	}
+	
+	private def _getTopics =
+	{
+	  val lines = Source.fromFile(TOPIC_PATH).getLines
+	  val topicsTitle = lines.filter(l => l.contains("<title>")).map(l => l.split(":")(1).trim)
+	  val topicsNumber = lines.filter(l => l.contains("<num>")).map(l => l.split(":")(1).trim.toInt)
+	  
+	  topicsTitle.zip(topicsNumber)
 	}
 	
 	private def _ordering(row: (String, Double)) = row._2
