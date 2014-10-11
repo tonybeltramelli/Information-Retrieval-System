1f369643dffc9538800b780650221beb6d046574
Tony Beltramelli <tbeltramelli@gmail.com>
2014-10-11 16:28:21 +0200

    update

--- src/com/tonybeltramelli/desktop/Main.scala
+++ src/com/tonybeltramelli/desktop/Main.scala
@@ -3,6 +3,7 @@ package com.tonybeltramelli.desktop
 import com.tonybeltramelli.desktop.util.ZipExplorer
 import ch.ethz.dal.tinyir.io.ZipDirStream
 import ch.ethz.dal.tinyir.io.TipsterStream
+import ch.ethz.dal.tinyir.processing.Tokenizer
 
 object Main {
 	def main(args: Array[String])
@@ -13,42 +14,33 @@ object Main {
 
 class Main
 {
-	//private val _zipDocStream = new ZipDirStream("data/tipster/zips")
-
 	def this(queries: Array[String])
 	{
 	  this()
 	  
-	  val t1 = System.nanoTime()
+	  val tipster = new TipsterStream("data/tipster/zips")
+	  //val queryTerms = Tokenizer.tokenize(query)
 	  
-	  val tipster = new TipsterStream("data/tipster/zips")  
-	  
-	  val t2 = System.nanoTime()
-	  
-	  println("stream with "+tipster.length+" documents")
+	  println("queries : "+queries.mkString(", "))
+	  println("documents : "+tipster.length)
 	  
 	  var length : Long = 0 
-    var tokens : Long = 0
-    for (doc <- tipster.stream.take(10000)) { 
-      length += doc.content.length          
-      tokens += doc.tokens.length
-    }
-    println("Final number of characters = " + length)
-    println("Final number of tokens     = " + tokens)
-    
-	  println("time : "+(t2 - t1)/1000000000.0+"s")
+	  var tokens : Long = 0
+	  for (doc <- tipster.stream.take(1000)) { 
+		  length += doc.content.length          
+		  tokens += doc.tokens.length
+	  }
+	  
+	  println("Final number of characters = " + length)
+	  println("Final number of tokens = " + tokens) 
 	  
 	  /*
-	  println("queries : "+queries.mkString(", "))
-	  println("stream with "+_getStream.length+" documents")
+	  val zipDocStream = new ZipDirStream("data/tipster/zips")
+	  println("stream with "+zipDocStream.stream.length+" documents")
 	  
 	  val zipExplorer = new ZipExplorer("data/tipster/zips")
 	  
 	  println(zipExplorer.all.length)
 	  println(zipExplorer.all.mkString("\n"))*/
 	}
-	/*
-	private def _getStream = {
-	  _zipDocStream.stream
-	}*/
 }
