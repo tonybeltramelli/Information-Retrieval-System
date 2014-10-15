d8b01dab18bc5d98dc3e04a9385cf00f225bccba
Tony Beltramelli <tbeltramelli@gmail.com>
2014-10-15 22:15:28 +0200

    add missing file

---
+++ src/com/tonybeltramelli/desktop/core/PrecRec.scala
@@ -0,0 +1,27 @@
+package com.tonybeltramelli.desktop.core
+
+import scala.collection.Seq
+import scala.math.max
+
+import ch.ethz.dal.tinyir.lectures.PrecisionRecall
+
+class PrecRec[A] (ranked: Seq[A], relev: Set[A]) extends PrecisionRecall[A] (ranked: Seq[A], relev: Set[A])
+{ 
+  override def precAt(recall: Double, interpolated: Boolean = false) = {
+    assert(recall >= 0.0 && recall <= 1.0)
+    val n = max(1, recall2num(recall))
+
+    if (interpolated) {
+      if (iprecs.length > 0) iprecs(n - 1) else 0
+    } else {
+      if (precs.length > 0) precs(n - 1) else 0
+    }
+  }
+}
+
+object PrecRec
+{
+  def evaluate[A] (retriev: Set[A], relev: Set[A]) = {
+    PrecisionRecall.evaluate(retriev, relev)
+  }
+}
