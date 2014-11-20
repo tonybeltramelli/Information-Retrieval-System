7168a47f6f2739551713291a2432e58a5370c099
Tony Beltramelli <tbeltramelli@gmail.com>
2014-11-20 22:18:23 +0100

    update logistic regression

--- src/com/tonybeltramelli/desktop/core/classifier/LogisticRegression.scala
+++ src/com/tonybeltramelli/desktop/core/classifier/LogisticRegression.scala
@@ -5,8 +5,22 @@ import scala.collection.mutable.ListBuffer
 
 class LogisticRegression extends AClassifier
 {
+  private val _classifiers: MutMap[String, BinaryClassifierLR] = MutMap() //class name -> binary classifier
   private val _documentFreq : MutMap[String, Int] = MutMap()
-  private var _theta: Array[Double] = Array(0.0)
+  
+  override def train(topic: String)
+  {
+    val bc = new BinaryClassifierLR
+    
+    for(docIndex <- _classesToDoc(topic))
+    {
+      val doc = _documents(docIndex)
+      
+      bc.feed(doc._1.map(f => f._2.toDouble).toArray)
+    }
+   
+    _classifiers += topic -> bc
+  }
   
   def trains(documentName: String, tokens: List[String], classCodes : Set[String])
   {
@@ -14,15 +28,22 @@ class LogisticRegression extends AClassifier
     
     val documentFeatures = _getDocumentFeatures(tokens)
     
-    _theta = _theta ++ _update(_theta, documentFeatures, true)
-    
     _documentCounter += 1
   }
   
   override def apply(tokens: List[String]) =
   {
-    _logistic(_theta, _getDocumentFeatures(tokens))
-    Set("")
+    var results : Set[String] = Set()
+    val documentFeatures = _getDocumentFeatures(tokens)
+    
+    for(bc <- _classifiers)
+    {
+      val p = bc._2.getProb(documentFeatures)
+      
+      if(p >= 0.5) results = results + bc._1
+    }
+    
+    results
   }
   
   private def _getDocumentFeatures(document: List[String]) = _getTermFreq(document).map(f => f._2.toDouble).toArray
@@ -36,43 +57,58 @@ class LogisticRegression extends AClassifier
   {
     _documentFreq ++= document.distinct.map(t => t -> (1 + _documentFreq.getOrElse(t, 0)))
   }
-    
-  private def _update(theta: Array[Double], documentFeatures: Array[Double], isRelated: Boolean) =
-  {
-    _vectorAddition(_gradientTheta(theta, documentFeatures, isRelated), theta)
-  }
   
-  private def _gradientTheta(theta: Array[Double], documentFeatures: Array[Double], isRelated: Boolean) =
+  class BinaryClassifierLR
   {
-    var chain = 0.0
+    private var _theta: Array[Double] = Array(0.0)
     
-    isRelated match
+    def feed(documentFeatures: Array[Double])
     {
-      case true => chain = 1 - _logistic(theta, documentFeatures)
-      case false => chain = - _logistic(theta, documentFeatures)
+      _theta = _theta ++ _update(_theta, documentFeatures, true)
     }
     
-    _scalarMultiplication(documentFeatures, chain)
-  }
+    def getProb(documentFeatures: Array[Double]) =
+    {
+      _logistic(_theta, documentFeatures)
+    }
   
-  private def _logistic(theta: Array[Double], documentFeatures: Array[Double]) =
-  {
-     1.0 / (1.0 + Math.exp(- _scalarProduct(documentFeatures, theta)))
-  }
+    private def _update(theta: Array[Double], documentFeatures: Array[Double], isRelated: Boolean) =
+    {
+      _vectorAddition(_gradientTheta(theta, documentFeatures, isRelated), theta)
+    }
   
-  private def _scalarProduct(vector1: Array[Double], vector2: Array[Double]) =
-  {
-    vector1.zip(vector2).map(v => v._1 * v._2).reduce(_ + _)
-    //vector1.map(v => v._2 * vector2.getOrElse(v._1, 0.0)).sum
-  }
+    private def _gradientTheta(theta: Array[Double], documentFeatures: Array[Double], isRelated: Boolean) =
+    {
+      var chain = 0.0
+    
+      isRelated match
+      {
+        case true => chain = 1 - _logistic(theta, documentFeatures)
+        case false => chain = - _logistic(theta, documentFeatures)
+      }
+    
+      _scalarMultiplication(documentFeatures, chain)
+    }
   
-  private def _scalarMultiplication(vector: Array[Double], scalar: Double) =
-  {
-    vector.map(v => v * scalar)
-  }
+    private def _logistic(theta: Array[Double], documentFeatures: Array[Double]) =
+    {
+      1.0 / (1.0 + Math.exp(- _scalarProduct(documentFeatures, theta)))
+    }
   
-  private def _vectorAddition(vector1: Array[Double], vector2: Array[Double]) =
-  {
-    vector1.zip(vector2).map(v => v._1 + v._2)
+    private def _scalarProduct(vector1: Array[Double], vector2: Array[Double]) =
+    {
+      vector1.zip(vector2).map(v => v._1 * v._2).reduce(_ + _)
+      //vector1.map(v => v._2 * vector2.getOrElse(v._1, 0.0)).sum
+    }
+  
+    private def _scalarMultiplication(vector: Array[Double], scalar: Double) =
+    {
+      vector.map(v => v * scalar)
+    }
+  
+    private def _vectorAddition(vector1: Array[Double], vector2: Array[Double]) =
+    {
+      vector1.zip(vector2).map(v => v._1 + v._2)
+    }
   }
 }
